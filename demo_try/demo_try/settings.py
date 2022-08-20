2fec31da26c0d2b5a8cb008c0e1754c811bf1525
raph651 <raph651@uw.edu>
2022-08-20 00:39:30 -0700

    backend update to upload/downloading via AWS S3

--- demo_try/demo_try/settings.py
+++ demo_try/demo_try/settings.py
@@ -12,10 +12,25 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+
+
+AWS_STORAGE_BUCKET_NAME = 'demotry-content'
+AWS_S3_CUSTOM_DOMAIN = AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com'
+AWS_S3_FILE_OVERWRITE = False
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl':'max-age=86400',
+                'ContentDisposition': 'attachment',
+}
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-MEDIA_URL ='/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = AWS_S3_CUSTOM_DOMAIN+ '/media/'
+MEDIA_ROOT = AWS_S3_CUSTOM_DOMAIN
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
