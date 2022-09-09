b47db7dbb25243cfd9a7c1e3fe4d8103f363ea7f
raph651 <raph651@uw.edu>
2022-09-09 15:30:16 -0700

    employee update functionality

--- frontend/hello/src/App.js
+++ frontend/hello/src/App.js
@@ -1,19 +1,67 @@
-import './App.css';
+import "./index.css";
+import Employee from "./components/Employee";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
+  const [employees, setEmployees] = useState([
+    {
+      id: 1,
+      name: "Raph",
+      role: "Intern",
+      img: "https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg",
+    },
+    {
+      id: 2,
+      name: "Raph",
+      role: "Intern",
+      img: "https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg",
+    },
+    {
+      id: 3,
+      name: "Raph",
+      role: "Intern",
+      img: "https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg",
+    },
+  ]);
+
+  function updateEmployee(id, newName, newRole) {
+    const updatedEmployee = employees.map((employee) => {
+      if (id == employee.id) {
+        return { ...employee, name: newName, role: newRole };
+      }
+      return employee;
+    });
+
+    setEmployees(updatedEmployee);
+  }
+
+  const showEmployee = true;
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Hello</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {showEmployee ? (
+        <>
+          <div className="absolute top-20 left-20">
+            <strong classname="bg-red-200">People</strong>
+            <div className="relative right-50 flex flex-col flex-wrap">
+              {employees.map((employee) => {
+                return (
+                  <Employee
+                    key={employee.id}
+                    id={employee.id}
+                    name={employee.name}
+                    role={employee.role}
+                    img={employee.img}
+                    updateEmployee={updateEmployee}
+                  />
+                );
+              })}
+            </div>
+          </div>
+        </>
+      ) : (
+        <p>You cannot see the employees</p>
+      )}
     </div>
   );
 }
